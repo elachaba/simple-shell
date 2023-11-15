ef61f00ca523aa80dada4810b947168c500bf401
elachaba <elachabaymane995@gmail.com>
2023-11-14 17:10:15 -0800

    parsing

--- mem_alloc.c
+++ mem_alloc.c
@@ -5,30 +5,30 @@
  * mem_err - handles memory error
  * Let's the system know that the last error that occurred
  * is a memory error
- * 
+ *
  * Return: void
 */
-void mem_err()
+void mem_err(void)
 {
-    errno = ENOMEM;
-    perror(0);
-    exit(EXIT_FAILURE);
+	errno = ENOMEM;
+	perror(0);
+	exit(EXIT_FAILURE);
 }
 
 /**
  * guarded_malloc - allocates memory and checks if the malloc
  * executed correctly
  * @size: the size of the memory to allocate
- * 
+ *
  * Return: a pointer to the allocated memory space
 */
 void *guarded_malloc(size_t size)
 {
-    void *ret = malloc(size);
+	void *ret = malloc(size);
 
-    if (!ret)
-        mem_err();
-    return (ret);
+	if (!ret)
+		mem_err();
+	return (ret);
 }
 
 /**
@@ -36,14 +36,14 @@ void *guarded_malloc(size_t size)
  * the realloc executed correctly
  * @ptr: a pointer to the current allocated memory area
  * @size: the size of the new memory area to allocate
- * 
+ *
  * Return: a pointer to the new allocated memory area
 */
 void *guarded_realloc(void *ptr, size_t size)
 {
-    void *ret = realloc(ptr, size);
+	void *ret = realloc(ptr, size);
 
-    if(!ret)
-        mem_err();
-    return (ret);
+	if (!ret)
+		mem_err();
+	return (ret);
 }
--- readcmd.c
+++ readcmd.c
@@ -13,38 +13,133 @@
 /**
  * readline - reads line from stdin and stores it in a char *
  * @prompt: the prompt printed on the screen
- * 
+ *
  * Return: the line read from stdin
 */
 
 char *readline(char *prompt)
 {
-    size_t buf_size = BUFF_SIZE;
-    char *buffer = guarded_malloc(buf_size * sizeof(char));
+	size_t buf_size = BUFF_SIZE;
+	char *buffer;
 
-    printf(prompt);
-    if (fgets(buffer, buf_size, stdin) == NULL)
-    {
-        free(buffer);
-        return (NULL);
-    }
+	buffer = guarded_malloc(buf_size * sizeof(char));
 
-    do {
-        size_t len_buf = strlen(buffer);
-        if ((len_buf > 0) && buffer[len_buf - 1] != '\n')
-        {
-            len_buf--;
-            buffer[len_buf] = 0;
-            return (buffer);
-        }
+	printf(prompt);
+	if (fgets(buffer, buf_size, stdin) == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
 
-        if (len_buf >= (INT_MAX / 2))
-            mem_err();
-        buf_size *= 2;
-        guarded_realloc(buffer, buf_size * sizeof(char));
+	do {
+		size_t len_buf = strlen(buffer);
 
-        if (fgets(buffer, buf_size, stdin) == NULL);
+		if ((len_buf > 0) && buffer[len_buf - 1] != '\n')
+		{
+			len_buf--;
+			buffer[len_buf] = 0;
+			return (buffer);
+		}
 
-    } while (1);
+		if (len_buf >= (INT_MAX / 2))
+			mem_err();
+		buf_size *= 2;
+		guarded_realloc(buffer, buf_size * sizeof(char));
 
-} 
+		if (fgets(buffer, buf_size, stdin) == NULL)
+			return (buffer);
+
+	} while (1);
+
+}
+
+/**
+ * split_in_words - Split the string in words, according
+ * to the simple shell grammar
+ * @line: the line to split
+ *
+ * Return: a list of strings containing the words in @line
+*/
+static char *split_in_words(char *line)
+{
+	char **tab = NULL, *delimiters = " \t&><|", *token, *del_str;
+	size_t l = 0;
+
+	while (*line != '\0')
+	{
+		if (strchr(delimiters, *line) != NULL)
+		{
+			del_str = guarded_malloc(2 * sizeof(char));
+			del_str[0] = *line;
+			del_str[1] = '\0';
+		}
+		tab = guarded_realloc(tab, (l + 1) * sizeof(char *));
+		tab[l++] = strdup(token);
+		line++;
+		else
+		{
+			token = strtok(line, delimiters);
+			if (token)
+			{
+				tab = guarded_realloc(tab, (l + 1) * sizeof(char *));
+				tab[l++] = strdup(token);
+				line = NULL;
+			}
+			else
+				break;
+		}
+	}
+
+	tab = guarded_realloc(tab, (l + 1) * sizeof(char *));
+	tab[l] = NULL;
+	return (tab);
+}
+
+/**
+ * parsecmd - parses the commands passed in the command line
+ * @pline:  the line read from the stdin
+ *
+ * Return: the information about the commands
+ */
+struct cmdline *parsecmd(char **pline)
+{
+	char *line = *pline, char **cmd, char ***seq, *w;
+	struct cmdline *s;
+	int i = 0;
+	size_t cmd_len = 0 seq_len = 0;
+
+	if (!line)
+	{
+		if (s)
+		{
+			freecmd(s);
+			free(s);
+		}
+		return (NULL);
+	}
+	cmd = guarded_malloc(sizeof(char *));
+	cmd[0] = NULL;
+	seq = guarded_malloc(sizeof(char **));
+	seq[0] = NULL;
+	words = split_in_words(line);
+	init_cmdline(s);
+	while ((w = word[i++]) != 0)
+	{
+		if (strchr("<>|&", w[0]))
+			handle_special(w, words[i]);
+		else
+			update_cmd(cmd, cmd_len);
+	}
+	if (cmd_len != 0)
+		update_seq(seq, seq_len);
+	else if (seq_len != 0)
+	{
+		s->err = "misplaced pipe";
+		i--;
+	}
+	else
+		free(cmd);
+	free(words);
+	s->seq = seq;
+	return (s);
+}
